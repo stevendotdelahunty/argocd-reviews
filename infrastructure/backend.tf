c996ee8d2626646438cb53881afdc9f4e83e670c
Steven Delahunty <steven.delahunty@smartpension.co.uk>
2022-08-12 14:28:36 +0100

    terraform fmt

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -2,10 +2,10 @@ terraform {
   required_version = "~> 1.2.6"
 
   backend "s3" {
-    acl            = "bucket-owner-full-control"
-    bucket         = "smartlabs-terraform-state"
-    encrypt        = true
-    region         = "eu-west-2"
-    key            = "eks/review-apps/eks-infra.tfstate"
+    acl     = "bucket-owner-full-control"
+    bucket  = "smartlabs-terraform-state"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "eks/review-apps/eks-infra.tfstate"
   }
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -91,25 +91,25 @@ variable "apply_config_map_aws_auth" {
 variable "instance_types" {
   type        = list(string)
   description = "Set of instance types associated with the EKS Node Group. Defaults to [\"t3.medium\"]. Terraform will only perform drift detection if a configuration value is provided"
-  default = ["t3.medium"]
+  default     = ["t3.medium"]
 }
 
 variable "desired_size" {
   type        = number
   description = "Desired number of worker nodes"
-  default = 1
+  default     = 1
 }
 
 variable "max_size" {
   type        = number
   description = "The maximum size of the AutoScaling Group"
-  default = 2
+  default     = 2
 }
 
 variable "min_size" {
   type        = number
   description = "The minimum size of the AutoScaling Group"
-  default = 1
+  default     = 1
 
 }
 
